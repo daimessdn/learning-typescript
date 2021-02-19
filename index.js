bf378a56babe87677038dc2d32b528ac59dd6c32
Dimas Wihandono <daimessdn@gmail.com>
2021-02-19 17:17:12 +0700

    first commit with prepared TypeScript and starter development node files

---
+++ index.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 2.8;
+var result = add(num1, num2);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,10 @@
+function add (n1: number, n2: number) {
+	return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = add(num1, num2);
+
+console.log(result);
