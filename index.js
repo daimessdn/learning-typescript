bda54bf03885512fc8c765901bcba30b8edb3d44
Dimas Wihandono <daimessdn@gmail.com>
2021-02-20 09:42:18 +0700

    added object.ts (how to use object type)

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var num1 = 5;
-var num2 = 2.8;
-var printResult = true;
-var phrase = "Result is ";
-add(num1, num2, printResult, phrase);
+var person = {
+    name: "Sarah",
+    age: 24
+};
+console.log(person.name);
