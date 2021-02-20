2c5f5a47aa0e0664c112c48bcc06a5589c855c43
Dimas Wihandono <daimessdn@gmail.com>
2021-02-20 10:16:40 +0700

    added enum.ts (how to use enum)

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 // re-defined the person object before
-//// with addition of role property: [number, string]
+//// with modification of role property: enum
+// defined enum Role
+// enum Role { ADMIN, READ_ONLY, AUTHOR };
+// //            0        1         2
+// you can specify the enum value
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 500] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
+//              "ADMIN"           500             "AUTHOR"         
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];         // tuple
+// } = {
 var person = {
     name: "Sarah",
     age: 24,
     hobbies: ["Cooking", "Reading"],
-    role: [2, "author"] // [number, string]
+    role: Role.ADMIN // enum Role
 };
-person.role.push("admin"); // EXCEPTION
-person.role[1] = 0; // VALID
-person.role = [0, "admin", "user"]; // ERROR only receives 2 elements [number, string]
+// person.role.push("admin");           // EXCEPTION
+// person.role[1] = 0;                  // ERROR element index 1 only receives string
+// person.role = [0, "admin", "user"];  // ERROR only receives 2 elements [number, string]
 console.log(person.role);
